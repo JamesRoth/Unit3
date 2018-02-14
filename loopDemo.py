af79f325501da91f2eb1974e82d974e6cdeff639
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-14 08:27:05 -0500

    Updated from Brython Server: 2/14/2018 8:27:04 AM

--- loopDemo.py
+++ loopDemo.py
@@ -7,21 +7,38 @@
 for i in range(1,6):
     print("I love computer science")
 """
+
+"""
+#while loop version:
 i=0
 while i<5:
     print("I love computer science")
+    i=i+1
+"""
+
 """
 #Counts from 1-10:
 for i in range (1,11):
     print(i)
 """
+#while loop version:
+i=1
+while i<=10:
+    print(1)
+    i+=1 #means add one to variable i
+
+"""
 #Count from 27-43 by 2s:
 for i in range(27,44,2):
+    print(i)
+"""
+
 """
 #add up the numbers 1-5:
 total=0
 for i in range (1,6):
     total=total+i
 print(total)
-""""
+"""
+
 
