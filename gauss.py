606bb801b82720c09deacb5b20f9f1322f85130d
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-15 08:25:29 -0500

    Updated from Brython Server: 2/15/2018 8:25:29 AM

--- gauss.py
+++ gauss.py
@@ -2,4 +2,7 @@
 #2/15/18
 #gauss.py - adding all numbers from 1-100
 
+range1=int(input("What number would you like to start at: "))
+range2=int(input("What number would you like to end at: "))
 
+for i in range(range1, range2+1)
