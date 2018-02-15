d50922ab8d68cc34d71e6f7b98927c5abef4cd8b
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-15 08:33:56 -0500

    Updated from Brython Server: 2/15/2018 8:33:55 AM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,11 @@
+#James Roth
+#2/15/18
+#hitchhiker.py - breaking loops
+
+while True:
+    ans=input("Ask a question or type quit: ")
+    if ans=="quit":
+        break
+    else:
+        print("42")
 
