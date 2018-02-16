fd924a7a35a1572b338662384472a11a586aca33
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-16 08:21:42 -0500

    Updated from Brython Server: 2/16/2018 8:21:42 AM

--- warmup6.py
+++ warmup6.py
@@ -1,7 +1,11 @@
 #James Roth
 #2/16/18
 #warmup6.py - for and while loop practice
-
+"""
 for i in range(1,11):
     print(i, "mississippi")
-
+"""
+i=0
+while i<11:
+    print(1, "mississippi")
+    i+=1
