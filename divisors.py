7f1e29465a98905f23fcf997cfe8d95c3ee6b223
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-26 08:31:47 -0500

    Updated from Brython Server: 2/26/2018 8:31:47 AM

--- divisors.py
+++ divisors.py
@@ -2,4 +2,9 @@
 #2/26/18
 #divisors.py - entering a number and priting its divisors
 
+num=int(input("Enter an interger: "))
 
+for i in range (1, num/2+1):
+    if num%i==0:
+        print(i)
+    
