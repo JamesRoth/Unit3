dae23fcfc4af8d38c873984b4e485b8ce3ceed98
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-26 08:39:57 -0500

    Updated from Brython Server: 2/26/2018 8:39:56 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,13 @@
+#James Roth
+#2/26/18
+#perfectNumber.py - does a number add up to its divisors
 
+num=int(input("Enter an integer: "))
+total=0
+for i in range (1, num//2+1):
+    if num%i==0:
+        total=total+i
+if total=num:
+    print(num, "is a perfect number!")
+else:
+    print(num, "is not a perfect number.")
