f517aab578954da0bea4f5f7864bfb01ee607e42
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-26 08:16:22 -0500

    Updated from Brython Server: 2/26/2018 8:16:22 AM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,22 @@
+#James Roth
+#2/26/18
+#numberGuessingGame.py - guessing a number
 
+from random import randint
+timesguessed=1
+num=randint(1,100)
+guess=int(input("Guess an integer between 1 and 100: "))
+
+while guess!=num:
+    print("Try again.")
+    timesguessed+=
+
+
+
+
+if num==guess:
+    print("You got it! It took you", timesguessed, "tries.")
+else:
+    print("Try again.")
+    
+    
