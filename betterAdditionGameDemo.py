514b2c429bfd486fe6a820e9960bd28b4ba019c2
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-28 08:20:15 -0500

    Updated from Brython Server: 2/28/2018 8:20:13 AM

--- betterAdditionGameDemo.py
+++ betterAdditionGameDemo.py
@@ -3,17 +3,16 @@
 #betterAdditionGameDemo.py - the name says it all
 
 from random import randint
-
 numCorrect=0
 numIncorrect=0
 while numCorrect<5:
     num1=randint(-10,10)
     num2=randint(-10,10)
     print("You've had", numCorrect, "correct answers, and", numIncorrect, "incorrect answers.")
-    answer=int(input("What is the sum of " + str(num1) + " and " + str(num2) + "?"))
+    answer=int(input("What is the sum of " + str(num1) + " and " + str(num2) + "? "))
     if answer==num1+num2:
         print("Correct!")
         numCorrect+=1
     else:
-        print("Incorrect.")
+        print("Incorrect. The correct answer was ", str(num1+num2) + "." )
         numIncorrect+=1
