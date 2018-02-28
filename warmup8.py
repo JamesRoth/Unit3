3e35503c7afb655fe15457a40edf08e47e89f964
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-28 08:28:59 -0500

    Updated from Brython Server: 2/28/2018 8:28:58 AM

--- warmup8.py
+++ warmup8.py
@@ -3,8 +3,7 @@
 #warmup8.py - sum of all numbers below 100,000 that are divisible by 3, 10, 17
 
 total=0
-for i in range(3, 100001, 3):
+for i in range(30, 100001, 3):
     if i%10==0 and i%17==0:
         total+=i
 print(total)
-    
