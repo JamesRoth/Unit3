8571a61f0c9b1081f2033dc984491dea199ddae8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-01 10:36:21 -0500

    Updated from Brython Server: 3/1/2018 10:36:20 AM

--- fri13th.py
+++ fri13th.py
@@ -8,7 +8,7 @@ found=0
 monthCount=date.today().month
 yearCount=date.today().year
 
-while found<1000:
+while found<10000:
     if date.today().day>13: #does this month have a fri 13th
         if monthCount<12: #is it december?
             monthCount+=1
