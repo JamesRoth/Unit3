479e9155533b748ac6a0e022013edf1508b43aa2
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-01 08:41:25 -0500

    Updated from Brython Server: 3/1/2018 8:41:24 AM

--- fri13th.py
+++ fri13th.py
@@ -4,7 +4,20 @@
 
 from calendar import weekday
 from datetime import date
+found=0
+monthCount=0
+yearCount=0
 
+"""
 date.today().day
 date.today().month
 date.today().year
+weekday(year,month,day)
+"""
+
+while found<10:
+    if date.today().day>13:
+        monthCount
+    weekday(date.today().year+yearCount, date.today().month+monthCount, date.today().day)
+    
+    found=found+1
