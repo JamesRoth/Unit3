c3d71b53447f48cfc614015833322af334c9699d
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-01 08:12:34 -0500

    Updated from Brython Server: 3/1/2018 8:12:33 AM

--- dotsDemo.py
+++ dotsDemo.py
@@ -9,7 +9,7 @@ red=Color(0xff0000,1)
 dot=CircleAsset(25, LineStyle(1, red), red)
 
 for i in range (15): #putting a row of dots
-    for j in range (10):
+    for j in range (9):
         Sprite(dot, (10+i*60,10+60*j))
 
 App().run()
