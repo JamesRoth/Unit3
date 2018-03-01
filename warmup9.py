766e66a99da0fe31f0d7d23394333252dac44c0d
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-01 08:22:34 -0500

    Updated from Brython Server: 3/1/2018 8:22:33 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,12 @@
+#James Roth
+#3/1/18
+#warmup9.py - warm up
 
+text=input("Enter some text: ")
+
+string=""
+for ch in text:
+    if ch==o or ch==i or ch==e or ch==a or ch==u:
+        string=string+"ch.upper()"
+    else:
+        srting=string+"ch"
