e8a8eb93da26cfa9c0d91ea1c43af657f5674165
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-02 08:27:31 -0500

    Updated from Brython Server: 3/2/2018 8:27:30 AM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,12 @@
+#James Roth
+#3/2/18
+#changeComputerLoop.py - finding change w/ loops
 
+money=int(input("Input a number of cents: "))
+quaters=0
+dimes=0
+nickels=0
+pennies=0
+
+while money-25>0:
+    quaters+=1
