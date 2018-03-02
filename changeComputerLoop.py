7529e842518e312923b4aa9925ea00b944d2607f
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-02 08:29:24 -0500

    Updated from Brython Server: 3/2/2018 8:29:24 AM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -8,16 +8,16 @@ dimes=0
 nickels=0
 pennies=0
 
-while money-25>0:
+while money-25>=0:
     quaters+=1
     money-=25
-while money-10>0:
+while money-10>=0:
     dimes+=1
     money-=10
-while money-5>0:
+while money-5>=0:
     nickels+=1
     money-=5
-while money-1>0:
+while money-1>=0:
     pennies+=1
     money-=1
 print(quaters, dimes, nickels, pennies)
