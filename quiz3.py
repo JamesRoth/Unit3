3c23699e09a4834f9da2260fac21b0cd76257ebd
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-05 08:19:19 -0500

    Updated from Brython Server: 3/5/2018 8:19:19 AM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,17 @@
+#James Roth
+#3/5/18
+#quiz3.py
 
+num=-15
+while num<-8:
+    print(num)
+    num+=1
+for i in range(50, 17, 2):
+    print(i)
+total=0
+for i in range (-100, 1002, 2):
+    total=total+i
+print(total)
+str="a"
+while "alpaca" not in str:
+    str=input("enter some text: ")
